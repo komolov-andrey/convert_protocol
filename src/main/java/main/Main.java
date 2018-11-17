659a0488bb2fc9d30e29930f3cfecaf726f5dd63
komolov-andrey <komolov.1995@gmail.com>
2018-11-17 23:15:58 +0300

    init Jetty project

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+/**
+ *
+ * @author Андрюха
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
---
+++ src/main/java/templater/PageGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templater;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+/**
+ *
+ * @author Андрюха
+ */
+public class PageGenerator {
+    private static final String HTML_DIR = "src\\main\\webapp";
+
+    private static PageGenerator pageGenerator;
+    private final Configuration cfg;
+
+    public static PageGenerator instance() {
+        if (pageGenerator == null)
+            pageGenerator = new PageGenerator();
+        return pageGenerator;
+    }
+
+    public String getPage(String filename, Map<String, Object> data) {
+        Writer stream = new StringWriter();
+        try {
+            Template template = cfg.getTemplate(HTML_DIR + File.separator + filename);
+            template.process(data, stream);
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+        return stream.toString();
+    }
+
+    private PageGenerator() {
+        cfg = new Configuration();
+    }
+}
