bac0340c4586860eec40a555c72b925b213ef173
komolov-andrey <komolov.1995@gmail.com>
2018-11-17 23:31:47 +0300

    add config file

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,12 +10,48 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.io.FileInputStream;
+
 /**
  *
  * @author Андрюха
  */
 public class Main {
+
     public static void main(String[] args) throws Exception {
+
+        // Чтение файла config.property
+        Properties prop = new Properties();
+        FileInputStream input = null;
+
+        try {
+
+            input = new FileInputStream("config.property");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("http.port"));
+            System.out.println(prop.getProperty("tcp.dest.addr"));
+            System.out.println(prop.getProperty("tcp.dest.port"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /*
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -26,5 +62,6 @@ public class Main {
 
         server.start();
         server.join();
+         */
     }
 }
